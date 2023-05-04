1aa8fcfdcec813453cece4b15908a259ac52f68e
MattIPv4 <me@mattcowley.co.uk>
2023-05-04 02:47:29 +0100

    Set defaults for generics in filters

--- src/ambassadors/filters.ts
+++ src/ambassadors/filters.ts
@@ -4,17 +4,21 @@ type AmbassadorsData = Record<string, Ambassador>;
 
 // Plush
 
-export type AmbassadorWithPlush<Base extends Ambassador> = Base & {
+export type AmbassadorWithPlush<Base extends Ambassador = Ambassador> = Base & {
   plush: Exclude<Base["plush"], null>;
 };
 
-export type AmbassadorWithPlushKey<Items extends AmbassadorsData> = {
+export type AmbassadorWithPlushKey<
+  Items extends AmbassadorsData = typeof ambassadors
+> = {
   [K in keyof Items]: Items[K] extends AmbassadorWithPlush<Items[K]>
     ? K
     : never;
 }[keyof Items];
 
-export type AmbassadorsWithPlush<Items extends AmbassadorsData> = {
+export type AmbassadorsWithPlush<
+  Items extends AmbassadorsData = typeof ambassadors
+> = {
   [K in AmbassadorWithPlushKey<Items>]: Items[K] extends AmbassadorWithPlush<
     Items[K]
   >
@@ -22,38 +26,50 @@ export type AmbassadorsWithPlush<Items extends AmbassadorsData> = {
     : never;
 };
 
-export const isAmbassadorWithPlushKey = <Items extends AmbassadorsData>(
+export const isAmbassadorWithPlushKey = <
+  Items extends AmbassadorsData = typeof ambassadors
+>(
   key: string | number | symbol
 ): key is AmbassadorWithPlushKey<Items> =>
   ambassadors[key as keyof typeof ambassadors].plush !== null;
 
-export const isAmbassadorWithPlush = <Items extends AmbassadorsData>(
+export const isAmbassadorWithPlush = <
+  Items extends AmbassadorsData = typeof ambassadors
+>(
   ambassador: Items[keyof Items]
 ): ambassador is AmbassadorWithPlush<Items[keyof Items]> =>
   ambassador.plush !== null;
 
 // Active
 
-export type ActiveAmbassador<Base extends Ambassador> = Base & {
+export type ActiveAmbassador<Base extends Ambassador = Ambassador> = Base & {
   retired: null;
 };
 
-export type ActiveAmbassadorKey<Items extends AmbassadorsData> = {
+export type ActiveAmbassadorKey<
+  Items extends AmbassadorsData = typeof ambassadors
+> = {
   [K in keyof Items]: Items[K] extends ActiveAmbassador<Items[K]> ? K : never;
 }[keyof Items];
 
-export type ActiveAmbassadors<Items extends AmbassadorsData> = {
+export type ActiveAmbassadors<
+  Items extends AmbassadorsData = typeof ambassadors
+> = {
   [K in ActiveAmbassadorKey<Items>]: Items[K] extends ActiveAmbassador<Items[K]>
     ? ActiveAmbassador<Items[K]>
     : never;
 };
 
-export const isActiveAmbassadorKey = <Items extends AmbassadorsData>(
+export const isActiveAmbassadorKey = <
+  Items extends AmbassadorsData = typeof ambassadors
+>(
   key: string | number | symbol
 ): key is ActiveAmbassadorKey<Items> =>
   ambassadors[key as keyof typeof ambassadors].retired === null;
 
-export const isActiveAmbassador = <Items extends AmbassadorsData>(
+export const isActiveAmbassador = <
+  Items extends AmbassadorsData = typeof ambassadors
+>(
   ambassador: Items[keyof Items]
 ): ambassador is ActiveAmbassador<Items[keyof Items]> =>
   ambassador.retired === null;
