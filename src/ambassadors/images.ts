0b35ea95bc4b2b724613046d732176c1661e962e
MattIPv4 <me@mattcowley.co.uk>
2023-05-19 14:58:54 +0100

    Fix Oliver primary image position

--- src/ambassadors/images.ts
+++ src/ambassadors/images.ts
@@ -240,7 +240,7 @@ const ambassadorImages: {
     {
       src: oliverImage1,
       alt: "Oliver the Olive Egger Chicken",
-      position: "75% 50%",
+      position: "75% 0%",
     },
     {
       src: oliverImage2,
