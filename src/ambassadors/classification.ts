e979d578264892c9f31549d675cbc4d51877d0e2
mforsb <13108814+mforsb@users.noreply.github.com>
2023-06-03 01:59:51 +0200

    Add the taxonomic class to each ambassador
    Needed for new group by functionality

---
+++ src/ambassadors/classification.ts
@@ -0,0 +1,32 @@
+export enum Class {
+  Mammalia,
+  Aves,
+  Reptilia,
+  Amphibia,
+  Arachnida,
+  Diplopoda,
+  Insecta,
+  Malacostraca,
+}
+
+const classificationOrder = ["Mammals", "Birds", "Reptiles & Amphibians", "Invertebrates"] as const;
+
+export type Classification = (typeof classificationOrder)[number];
+
+export function getClassification(c: Class): Classification {
+  switch (c) {
+    case Class.Mammalia: return "Mammals";
+    case Class.Aves: return "Birds";
+    case Class.Reptilia:
+    case Class.Amphibia: return "Reptiles & Amphibians";
+    case Class.Arachnida:
+    case Class.Diplopoda:
+    case Class.Insecta:
+    case Class.Malacostraca: return "Invertebrates";
+  }
+}
+
+export function sortAmbassadorClassification(a: Class, b: Class): number {
+  return classificationOrder.indexOf(getClassification(a)) - classificationOrder.indexOf(getClassification(b));
+}
+
