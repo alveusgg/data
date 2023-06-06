ddf3ebedefee1c82e1b2057d86767323c4a08a42
mforsb <13108814+mforsb@users.noreply.github.com>
2023-06-06 23:42:48 +0200

    Run `pnpm run format`

--- src/ambassadors/classification.ts
+++ src/ambassadors/classification.ts
@@ -9,23 +9,35 @@ export enum Class {
   Malacostraca,
 }
 
-const classificationOrder = ["Mammals", "Birds", "Reptiles & Amphibians", "Invertebrates"] as const;
+const classificationOrder = [
+  "Mammals",
+  "Birds",
+  "Reptiles & Amphibians",
+  "Invertebrates",
+] as const;
 
 export type Classification = (typeof classificationOrder)[number];
 
 export const getClassification = (c: Class): Classification => {
   switch (c) {
-    case Class.Mammalia: return "Mammals";
-    case Class.Aves: return "Birds";
+    case Class.Mammalia:
+      return "Mammals";
+    case Class.Aves:
+      return "Birds";
     case Class.Reptilia:
-    case Class.Amphibia: return "Reptiles & Amphibians";
+    case Class.Amphibia:
+      return "Reptiles & Amphibians";
     case Class.Arachnida:
     case Class.Diplopoda:
     case Class.Insecta:
-    case Class.Malacostraca: return "Invertebrates";
+    case Class.Malacostraca:
+      return "Invertebrates";
   }
-}
+};
 
 export const sortAmbassadorClassification = (a: Class, b: Class): number => {
-  return classificationOrder.indexOf(getClassification(a)) - classificationOrder.indexOf(getClassification(b));
-}
+  return (
+    classificationOrder.indexOf(getClassification(a)) -
+    classificationOrder.indexOf(getClassification(b))
+  );
+};
