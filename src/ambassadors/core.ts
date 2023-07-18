8c1a1daf45a807622cacf86b03add59161748616
jayway <70596906+jibranabsarulislam@users.noreply.github.com>
2023-07-17 21:34:25 -0400

    Incorporate native habitat data and info sources (#22)
    * add native habitat descriptions
    
    * expand native into text and source fields
    
    * run format
    
    * version update with npm version --no-git-tag-version minor
    
    * domestic rat instead of rat species (wild)
    
    * remove double pasted link
    
    Co-authored-by: Matt Cowley <me@mattcowley.co.uk>
    
    * check and add (Domesticated) disclaimer
    
    * run minor version update
    
    * put back isopod status
    
    * Update src/ambassadors/core.ts
    
    Co-authored-by: Matt Cowley <me@mattcowley.co.uk>
    
    ---------
    
    Co-authored-by: Matt Cowley <me@mattcowley.co.uk>

--- src/ambassadors/core.ts
+++ src/ambassadors/core.ts
@@ -36,6 +36,10 @@ export type Ambassador = {
   enclosure: EnclosureKey;
   story: string;
   mission: string;
+  native: {
+    text: string;
+    source: string;
+  };
   clips: Readonly<{ id: string; caption: string }[]>;
   homepage: Nullable<{ title: string; description: string }>;
   plush: Nullable<{ link: string } | { soon: string }>;
@@ -62,6 +66,10 @@ const ambassadors = {
       "Stompy was hatched in captivity and was hand raised by Maya to be the first Alveus ambassador.",
     mission:
       "Stopping exotic meat trade, traditional medicine, and over exploitation of animal products in cosmetics.",
+    native: {
+      text: "Australia (savannah woodlands and sclerophyll forests)",
+      source: "https://en.wikipedia.org/wiki/Emu",
+    },
     clips: [
       {
         id: "GsBRS13e8mY",
@@ -108,6 +116,10 @@ const ambassadors = {
       "Georgie was part of an educational program at a zoo and was rehomed to Alveus.",
     mission:
       "He is an ambassador for the wildlife trade and how chytrid fungus is affecting amphibian species worldwide.",
+    native: {
+      text: "Sub-Saharan Africa (dry savanna, freshwater lakes, marshes, etc.)",
+      source: "https://en.wikipedia.org/wiki/African_bullfrog",
+    },
     clips: [
       { id: "4WpuyhCXTTs", caption: "Georgie's Journey At Alveus" },
       { id: "YNHssqn1O40", caption: "Georgie Gets Fed By ConnorEatsPants" },
@@ -143,6 +155,10 @@ const ambassadors = {
       "Tico was rehomed to Alveus as the previous sanctuary’s owner was retiring,",
     mission:
       "She is an ambassador for how the pet trade has affected her parrot species and many others around the world.",
+    native: {
+      text: "South America (tropical rainforests)",
+      source: "https://en.wikipedia.org/wiki/Blue-and-yellow_macaw",
+    },
     clips: [
       { id: "7Zqpoxpm0aQ", caption: "Training With Tico And Kayla" },
       { id: "du1TIuJ6BAk", caption: "Tico Dancing With Kayla" },
@@ -169,6 +185,10 @@ const ambassadors = {
       "Miley was rehomed to Alveus as the previous sanctuary’s owner was retiring.",
     mission:
       "She is an ambassador for how the pet trade has affected her parrot species and many others around the world.",
+    native: {
+      text: "Usually bred in captivity",
+      source: "https://en.wikipedia.org/wiki/Catalina_macaw",
+    },
     clips: [{ id: "HEcrzoXofB8", caption: "Maya Gives Miley A Bath" }],
     homepage: null,
     plush: null,
@@ -192,6 +212,10 @@ const ambassadors = {
       "Mia was rehomed to Alveus as the previous sanctuary’s owner was retiring.",
     mission:
       "She is an ambassador for how the pet trade has affected her parrot species and many others around the world.",
+    native: {
+      text: "Equatorial Africa (dense forests)",
+      source: "https://en.wikipedia.org/wiki/African_grey_parrot",
+    },
     clips: [
       { id: "nIw-qTRT5Nc", caption: "Mia Dances With Connor" },
       { id: "KRJy_nR1u5k", caption: "Mia Walks Around The Aviary" },
@@ -221,6 +245,10 @@ const ambassadors = {
       "Siren was rehomed to Alveus as the previous sanctuary’s owner was retiring.",
     mission:
       "She is an ambassador for how the pet trade has affected her parrot species and many others around the world.",
+    native: {
+      text: "South America (tropical rainforests)",
+      source: "https://en.wikipedia.org/wiki/Blue-fronted_amazon",
+    },
     clips: [
       { id: "ociryM83xIo", caption: "Siren Is Given Enrichment" }, // This is a Maya clip
       { id: "ynv06rC9aHM", caption: "Siren Talks, Sings, And More" },
@@ -251,6 +279,10 @@ const ambassadors = {
       "Abbott was brought into a wildlife rehab center as a baby. He imprinted during the rehab process and then was deemed non-releasable.",
     mission:
       "He is an ambassador for educating people on the misconceptions that wildlife face as well as human-wildlife conflict.",
+    native: {
+      text: "North America (forests)",
+      source: "https://en.wikipedia.org/wiki/American_crow",
+    },
     clips: [
       { id: "GqvK9m6yoBQ", caption: "Abbott Explores His New Enrichment" },
     ],
@@ -276,6 +308,10 @@ const ambassadors = {
       "Coconut was brought into a wildlife rehab center as a baby. He imprinted during the rehab process and then was deemed non-releasable.",
     mission:
       "He is an ambassador for educating people on the misconceptions that wildlife face as well as human-wildlife conflict.",
+    native: {
+      text: "North America (forests)",
+      source: "https://en.wikipedia.org/wiki/American_crow",
+    },
     clips: [],
     homepage: null,
     plush: null,
@@ -298,6 +334,10 @@ const ambassadors = {
     story: "Oliver was put in the reject/return cage at a local feed store.",
     mission:
       "He is an ambassador for the agricultural industry and how people can use consumer choice to impact the environment in a positive way.",
+    native: {
+      text: "North America (domesticated)",
+      source: "https://en.wikipedia.org/wiki/Chicken",
+    },
     clips: [],
     homepage: null,
     plush: null,
@@ -321,6 +361,10 @@ const ambassadors = {
       "Nugget was rehomed to Alveus because she was bullied by hens in her previous flock.",
     mission:
       "She is an ambassador for the agricultural industry and how people can use consumer choice to impact the environment in a positive way.",
+    native: {
+      text: "North America (domesticated)",
+      source: "https://en.wikipedia.org/wiki/Chicken",
+    },
     clips: [{ id: "GcPu01J0G-Q", caption: "Nugget Belongs In A Circus" }],
     homepage: null,
     plush: null,
@@ -343,6 +387,10 @@ const ambassadors = {
     story: "Henrique was rehomed from a local farm.",
     mission:
       "She is an ambassador for the agricultural industry and how people can use consumer choice to impact the environment in a positive way.",
+    native: {
+      text: "North America (domesticated)",
+      source: "https://en.wikipedia.org/wiki/Chicken",
+    },
     clips: [],
     homepage: null,
     plush: null,
@@ -365,6 +413,10 @@ const ambassadors = {
     story: "Winnie came from a cattle operation in Oklahoma.",
     mission:
       "She is an ambassador for the beef industry and how people can use their consumer choice to impact the environment in a positive way.",
+    native: {
+      text: "Australia, United States (domesticated)",
+      source: "https://en.wikipedia.org/wiki/Red_Angus",
+    },
     clips: [
       { id: "FKU7-u5-L24", caption: "Winnie Causes Chaos In The Grain Stall" },
       { id: "qWDd5nnWSuI", caption: "Winnie's First Day On Stream" },
@@ -393,6 +445,10 @@ const ambassadors = {
       "Noodle was part of an educational program at a zoo and was rehomed to Alveus.",
     mission:
       "She is an ambassador for how the pet trade and habitat loss has affected hers and many other reptile species worldwide.",
+    native: {
+      text: "Australia (suburban area, rainforests, forests, etc.)",
+      source: "https://en.wikipedia.org/wiki/Carpet_python",
+    },
     clips: [
       {
         id: "SU3b-ndBZ7M",
@@ -422,6 +478,10 @@ const ambassadors = {
       "Patchy was rehomed to Alveus from a local breeder after it was discovered that she had a genetic defect and was missing an eye.",
     mission:
       "She is an ambassador for how the pet trade and habitat loss has affected hers and many other reptile species worldwide.",
+    native: {
+      text: "West Sub Saharan Africa (grasslands, savannas, sparsely wooded areas)",
+      source: "https://en.wikipedia.org/wiki/Ball_python",
+    },
     clips: [],
     homepage: null,
     plush: null,
@@ -445,6 +505,10 @@ const ambassadors = {
       "Fenn was confiscated from the illegal pet trade by California Department of Fish and Wildlife. He was then rehomed to Alveus.",
     mission:
       "He is an ambassador for the exploitation of wildlife in the pet trade and the fur trade.",
+    native: {
+      text: "North America, Europe, Asia, North Africa (forests, grasslands, mountains)",
+      source: "https://en.wikipedia.org/wiki/Red_fox",
+    },
     clips: [
       { id: "670RYU2Xw98", caption: "Fenn Digs Up A Friend In The Enclosure" },
       { id: "awX8PnOfhQA", caption: "Fenn Finds A Well Hidden Treasure" },
@@ -476,6 +540,10 @@ const ambassadors = {
       "Reed was orphaned as a wild kit and was rescued to be raised at a zoo to be an educational ambassador. He was then rehomed to Alveus.",
     mission:
       "He is an ambassador for the exploitation of wildlife in the pet trade and the fur trade.",
+    native: {
+      text: "North America, Europe, Asia, North Africa (forests, grasslands, mountains)",
+      source: "https://en.wikipedia.org/wiki/Red_fox",
+    },
     clips: [
       { id: "ClWtgsoWta0", caption: "Reed Gets Weighed With Target Training" },
       { id: "4HZMQJoEkVA", caption: "Reed Is The Best Boy" },
@@ -505,6 +573,10 @@ const ambassadors = {
     story: "Rehomed to Alveus from a local equine rescue.",
     mission:
       "Serrano is an ambassador for the wildlife trade and use of wild animals in traditional medicine.",
+    native: {
+      text: "Worldwide (domesticated)",
+      source: "https://en.wikipedia.org/wiki/Donkey",
+    },
     clips: [
       { id: "D0daZV1P3AM", caption: "Donkeys Fighting For The Ball" },
       { id: "81Zrv7veNMY", caption: "The Donkeys Play With A Ball At Night" },
@@ -532,6 +604,10 @@ const ambassadors = {
     story: "Rehomed to Alveus from a local equine rescue.",
     mission:
       "Jalapeño is an ambassador for the wildlife trade and use of wild animals in traditional medicine.",
+    native: {
+      text: "Worldwide (domesticated)",
+      source: "https://en.wikipedia.org/wiki/Donkey",
+    },
     clips: [
       { id: "D0daZV1P3AM", caption: "Donkeys Fighting For The Ball" },
       { id: "81Zrv7veNMY", caption: "The Donkeys Play With A Ball At Night" },
@@ -558,6 +634,10 @@ const ambassadors = {
       "Snork was part of an educational program at a zoo and was rehomed to Alveus.",
     mission:
       "She is an ambassador for the exploitation of wildlife in the fur trade.",
+    native: {
+      text: "Andes Mountains, South America (Domesticated)",
+      source: "https://en.wikipedia.org/wiki/Chinchilla",
+    },
     clips: [{ id: "djDIUl2c0v8", caption: "Moomin Steals A Stick From Snork" }],
     homepage: null,
     plush: null,
@@ -580,6 +660,10 @@ const ambassadors = {
     story: "Rehomed from a local pet owner.",
     mission:
       "He is an ambassador for the exploitation of wildlife in the fur trade.",
+    native: {
+      text: "Andes Mountains, South America (Domesticated)",
+      source: "https://en.wikipedia.org/wiki/Chinchilla",
+    },
     clips: [{ id: "djDIUl2c0v8", caption: "Moomin Steals A Stick From Snork" }],
     homepage: {
       title: "Moomin is Movin' In!",
@@ -607,6 +691,10 @@ const ambassadors = {
       "Our smokey ghost millipede was born in captivity and ethically sourced.",
     mission:
       "He is an ambassador for the importance of invertebrates and the misconceptions they face.",
+    native: {
+      text: "Eastern United States",
+      source: "https://en.wikipedia.org/wiki/Narceus_gordanus",
+    },
     clips: [
       { id: "eUOJbijrsCk", caption: "Hank Says Hi To Stream" },
       { id: "D-KMGuj3NfY", caption: "Hank Poops While Being Introduced" },
@@ -633,6 +721,10 @@ const ambassadors = {
       "Our cockroach colony was started with 7 roaches that were part of an educational colony at a school in Pennsylvania.",
     mission:
       "They are ambassadors for the importance of invertebrates and the misconceptions they face.",
+    native: {
+      text: "Madagascar",
+      source: "https://en.wikipedia.org/wiki/Gromphadorhina_portentosa",
+    },
     clips: [],
     homepage: null,
     plush: null,
@@ -655,6 +747,10 @@ const ambassadors = {
     story: "Our zebra isopods were born in captivity and ethically sourced.",
     mission:
       "They are ambassadors for the importance of invertebrates and the misconceptions they face.",
+    native: {
+      text: "Southern France",
+      source: "https://en.wikipedia.org/wiki/Armadillidium_maculatum",
+    },
     clips: [],
     homepage: null,
     plush: null,
@@ -678,10 +774,15 @@ const ambassadors = {
       "Our spanish orange isopods were born in captivity and ethically sourced.",
     mission:
       "They are ambassadors for the importance of invertebrates and the misconceptions they face.",
+    native: {
+      text: "Europe",
+      source: "https://en.wikipedia.org/wiki/Porcellio_scaber",
+    },
     clips: [],
     homepage: null,
     plush: null,
   },
+
   tortellini: {
     name: "Tortellini",
     alternate: [],
@@ -701,6 +802,10 @@ const ambassadors = {
       "Our emperor scorpions were born in captivity and ethically sourced.",
     mission:
       "He is an ambassador for the importance of invertebrates and the misconceptions they face.",
+    native: {
+      text: "Africa (rainforests, savannas)",
+      source: "https://en.wikipedia.org/wiki/Pandinus_imperator",
+    },
     clips: [],
     homepage: null,
     plush: null,
@@ -724,6 +829,10 @@ const ambassadors = {
       "Our emperor scorpions were born in captivity and ethically sourced.",
     mission:
       "He is an ambassador for the importance of invertebrates and the misconceptions they face.",
+    native: {
+      text: "Africa (rainforests, savannas)",
+      source: "https://en.wikipedia.org/wiki/Pandinus_imperator",
+    },
     clips: [],
     homepage: null,
     plush: null,
@@ -747,6 +856,10 @@ const ambassadors = {
       "Chips Ahoy was bred as a feeder rat for snakes, but she will now live out her full life at Alveus.",
     mission:
       "She is an ambassadors for how rodenticide use and outdoor cats affect all levels of wildlife.",
+    native: {
+      text: "Worldwide (Domesticated)",
+      source: "https://en.wikipedia.org/wiki/Fancy_rat",
+    },
     clips: [{ id: "C2Ob-gQ_9os", caption: "Target Training With The Rats" }],
     homepage: null,
     plush: null,
@@ -770,6 +883,10 @@ const ambassadors = {
       "Nilla Wafer was bred as a feeder rat for snakes, but she will now live out her full life at Alveus.",
     mission:
       "She is an ambassadors for how rodenticide use and outdoor cats affect all levels of wildlife.",
+    native: {
+      text: "Worldwide (Domesticated)",
+      source: "https://en.wikipedia.org/wiki/Fancy_rat",
+    },
     clips: [{ id: "C2Ob-gQ_9os", caption: "Target Training With The Rats" }],
     homepage: null,
     plush: null,
@@ -793,6 +910,10 @@ const ambassadors = {
       "Toaster Strudel was part of an educational program at a zoo and was rehomed to Alveus.",
     mission:
       "He is an ambassador for how human development and invasive species can affect natural habitats and native species.",
+    native: {
+      text: "Australia",
+      source: "https://en.wikipedia.org/wiki/Blue-tongued_skink",
+    },
     clips: [],
     homepage: null,
     plush: null,
@@ -816,6 +937,10 @@ const ambassadors = {
       "Momo was rescued by Alveus after being surrendered to a veterinarian due to neglect.",
     mission:
       "He is an ambassador for the exotic pet trade and how it affects primate species worldwide.",
+    native: {
+      text: "Brazil (Neo-tropical gallery forests)",
+      source: "https://en.wikipedia.org/wiki/Black-tufted_marmoset",
+    },
     clips: [
       { id: "kSC1O1Tqgz8", caption: "The Monkeys Make Their Debut On Stream" },
     ],
@@ -841,6 +966,10 @@ const ambassadors = {
       "Appa was rescued by Alveus after being surrendered to a veterinarian due to neglect.",
     mission:
       "He is an ambassador for the exotic pet trade and how it affects primate species worldwide.",
+    native: {
+      text: "Brazil",
+      source: "https://en.wikipedia.org/wiki/Common_marmoset",
+    },
     clips: [
       { id: "kSC1O1Tqgz8", caption: "The Monkeys Make Their Debut On Stream" },
     ],
@@ -868,6 +997,10 @@ const ambassadors = {
       "Orion was originally owned by Maya before becoming an ambassador at Alveus.\n\nOrion sadly passed away in June 2022 after an unfortunate accident in his enclosure.",
     mission:
       "He is an ambassador for showing how pest control affects wildlife globally and how to conserve major keystone species.",
+    native: {
+      text: "Western North America",
+      source: "https://en.wikipedia.org/wiki/Prairie_falcon",
+    },
     clips: [],
     homepage: null,
     plush: null,
@@ -891,6 +1024,10 @@ const ambassadors = {
       "Our vinegaroon was born in captivity and ethically sourced.\n\nSadly, Pickles passed away in February 2023 because she was unable to make it through her first molt.",
     mission:
       "She is an ambassador for the importance of invertebrates and the misconceptions they face.",
+    native: {
+      text: "Tropical and subtropical areas, excluding Europe and Australia",
+      source: "https://en.wikipedia.org/wiki/Vinegaroon",
+    },
     clips: [],
     homepage: null,
     plush: null,
@@ -914,6 +1051,10 @@ const ambassadors = {
       "Henrietta was rehomed from a local farm.\n\nHenrietta was retired from being an ambassador in March 2023 and now lives in a chicken coop at Maya's Tiny House with her rabbit and other chicken.",
     mission:
       "She is an ambassador for the agricultural industry and how people can use consumer choice to impact the environment in a positive way.",
+    native: {
+      text: "United States (Domesticated)",
+      source: "https://en.wikipedia.org/wiki/Jersey_Giant",
+    },
     clips: [],
     homepage: null,
     plush: null,
@@ -937,6 +1078,10 @@ const ambassadors = {
       "Polly was rehomed from another owner.\n\nWe had hoped that Polly would grow to be larger so she could join our other chickens, but unfortunately she did not and so was retired from being an ambassador in November 2022 and now lives at Maya's Tiny House.",
     mission:
       "She is an ambassador for the agricultural industry and how people can use consumer choice to impact the environment in a positive way.",
+    native: {
+      text: "China (Domesticated)",
+      source: "https://en.wikipedia.org/wiki/Silkie",
+    },
     clips: [],
     homepage: null,
     plush: null,
@@ -960,6 +1105,10 @@ const ambassadors = {
       "Our rubber ducky isopods were born in captivity and ethically sourced.\n\nUnfortunately, our colony of isopods didn't breed enough to sustain themselves, with the final few passing away in June 2023.",
     mission:
       "They are ambassadors for the importance of invertebrates and the misconceptions they face.",
+    native: {
+      text: "Asia",
+      source: "https://en.wikipedia.org/wiki/Cubaris",
+    },
     clips: [],
     homepage: null,
     plush: null,
