891183a21a6b406db5146de33214b37ca567d65f
MattIPv4 <me@mattcowley.co.uk>
2025-06-23 19:57:49 +0100

    Use ts-morph for script to perform proper AST manipulation

--- scripts/iucn.ts
+++ scripts/iucn.ts
@@ -1,23 +1,14 @@
 /// <reference types="@types/node" />
 
-import { writeFile, readFile } from "node:fs/promises";
-import { fileURLToPath } from "node:url";
-
+import { Project, SyntaxKind } from "ts-morph";
 import { z } from "zod/v4";
 
-import type { Species } from "../src/ambassadors/species";
 import { iucnStatusSchema } from "../src/iucn";
 
 const token = process.env.IUCN_API_TOKEN;
 if (!token) {
   throw new Error("IUCN_API_TOKEN is not set");
 }
-
-const path = new URL("../src/ambassadors/species.ts", import.meta.url);
-const updateFile = async (replacement: (content: string) => string) =>
-  readFile(path, "utf8")
-    .then(replacement)
-    .then((content) => writeFile(path, content));
 
 const lookup = async (endpoint: string) => {
   const response = await fetch(
@@ -80,63 +71,84 @@ const lookupAssessment = async (id: number) => {
     .parse(data);
 };
 
-// Mark the raw species data as exported
-await updateFile((content) =>
-  content.replace(/^const species = /m, "export const species = "),
-);
+const project = new Project();
+const file = project.addSourceFileAtPath("src/ambassadors/species.ts");
 
-// Import the newly exported species data
-const species = await import(fileURLToPath(path)).then(
-  (module) => module.species as Record<string, Species>,
-);
+const species = file
+  .getVariableDeclaration("species")
+  ?.getFirstDescendantByKind(SyntaxKind.ObjectLiteralExpression);
+if (!species) throw new Error("Could not find species variable declaration");
 
-// Check latest assessment for each species
-const updates: { id: number; assessment: number; status: string }[] = [];
-for (const value of Object.values(species)) {
-  if (!value.iucn.id) continue;
+const objects = species.getChildrenOfKind(SyntaxKind.PropertyAssignment);
+if (!objects.length)
+  throw new Error("No species found in the species variable declaration");
 
-  console.log(`Checking IUCN data for ${value.name} (${value.iucn.id})`);
-  const speciesData = await lookupSpecies(value.iucn.id);
+for (const node of objects) {
+  const obj = node.getFirstChildByKind(SyntaxKind.ObjectLiteralExpression);
+  if (!obj)
+    throw new Error("Could not find object literal in species declaration");
+
+  const name = obj
+    .getProperty("name")
+    ?.getFirstChildByKind(SyntaxKind.StringLiteral);
+  if (!name) throw new Error("Could not find name property in species object");
+
+  const iucn = obj
+    .getProperty("iucn")
+    ?.getFirstChildByKind(SyntaxKind.ObjectLiteralExpression);
+  if (!iucn) throw new Error("Could not find IUCN property in species object");
+
+  const id = iucn
+    .getProperty("id")
+    ?.getFirstChildByKind(SyntaxKind.NumericLiteral);
+  if (!id) continue; // Skip when ID is null
+
+  const assessment = iucn
+    .getProperty("assessment")
+    ?.getFirstChildByKind(SyntaxKind.NumericLiteral);
+  if (!assessment)
+    throw new Error(
+      `Could not find assessment property for ${name.getLiteralValue()} (${id.getLiteralValue()})`,
+    );
+
+  const status = iucn
+    .getProperty("status")
+    ?.getFirstChildByKind(SyntaxKind.StringLiteral);
+  if (!status)
+    throw new Error(
+      `Could not find status property for ${name.getLiteralValue()} (${id.getLiteralValue()})`,
+    );
+
+  console.log(
+    `Checking IUCN data for ${name.getLiteralValue()} (${id.getLiteralValue()})`,
+  );
+  const speciesData = await lookupSpecies(id.getLiteralValue());
   const assessmentId = speciesData.assessments.find(
     (a) => a.latest,
   )?.assessment_id;
-  if (!assessmentId) {
+  if (!assessmentId)
     throw new Error(
-      `No latest assessment found for species ${value.name} (${value.iucn.id})`,
+      `No latest assessment found for species ${name.getLiteralValue()} (${id.getLiteralValue()})`,
     );
-  }
 
   const assessmentData = await lookupAssessment(assessmentId);
-  let status = assessmentData.red_list_category.code;
-  if (assessmentData.population_trend?.code === "0") status += "/increasing";
-  if (assessmentData.population_trend?.code === "1") status += "/decreasing";
+  let assessmentStatus = assessmentData.red_list_category.code;
+  if (assessmentData.population_trend?.code === "0")
+    assessmentStatus += "/increasing";
+  if (assessmentData.population_trend?.code === "1")
+    assessmentStatus += "/decreasing";
 
-  if (assessmentId !== value.iucn.assessment || status !== value.iucn.status) {
+  if (
+    assessmentId !== assessment.getLiteralValue() ||
+    assessmentStatus !== status.getLiteralValue()
+  ) {
     console.log(
-      `Updating IUCN data for ${value.name} (${value.iucn.id}) from ${value.iucn.assessment} ${value.iucn.status} to ${assessmentId} ${status}`,
+      `Updating IUCN data for ${name.getLiteralValue()} (${id.getLiteralValue()}) from ${assessment.getLiteralValue()} ${status.getLiteralValue()} to ${assessmentId} ${assessmentStatus}`,
     );
-    updates.push({
-      id: value.iucn.id,
-      assessment: assessmentId,
-      status: status,
-    });
+
+    assessment.setLiteralValue(assessmentId);
+    status.setLiteralValue(assessmentStatus);
   }
 }
 
-// Apply updates to the species data, and revert the export
-await updateFile((content) => {
-  let updatedContent = content;
-  for (const update of updates) {
-    updatedContent = updatedContent.replace(
-      new RegExp(
-        `iucn: {([\\s\\n]+)id: ${update.id},\\1assessment: \\d+,\\1status: "[^"]+"`,
-        "m",
-      ),
-      `iucn: {$1id: ${update.id},$1assessment: ${update.assessment},$1status: "${update.status}"`,
-    );
-  }
-  return updatedContent.replace(
-    /^export const species = /m,
-    "const species = ",
-  );
-});
+await file.save();
