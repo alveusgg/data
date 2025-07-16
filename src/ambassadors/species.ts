f201d5efa9c5d6fdb56f5507dd6d54850ebba324
MattIPv4 <me@mattcowley.co.uk>
2025-07-16 02:38:53 +0100

    Simplify zod imports

--- src/ambassadors/species.ts
+++ src/ambassadors/species.ts
@@ -1,4 +1,4 @@
-import { z } from "zod/v4";
+import { z } from "zod";
 
 import { iucnStatusSchema } from "../iucn";
 import { classSchema } from "./classification";
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import { z } from "zod/v4";
+import { z } from "zod";
 
 type OneToNine = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 type ZeroToNine = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
