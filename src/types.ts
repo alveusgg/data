1f63e731d10db717bb53ad2f6000c6d9b5b89f11
MattIPv4 <me@mattcowley.co.uk>
2026-07-01 14:06:58 +0100

    Fix Prettier formatting

--- src/types.ts
+++ src/types.ts
@@ -3,16 +3,13 @@ import { z } from "zod";
 type OneToNine = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 type ZeroToNine = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 type DateStringYear =
-  | `19${ZeroToNine}${ZeroToNine}`
-  | `20${ZeroToNine}${ZeroToNine}`;
+  `19${ZeroToNine}${ZeroToNine}` | `20${ZeroToNine}${ZeroToNine}`;
 type DateStringMonth = `0${OneToNine}` | `1${0 | 1 | 2}`;
 type DateStringYearMonth = `${DateStringYear}-${DateStringMonth}`;
 type DateStringDay = `${0}${OneToNine}` | `${1 | 2}${ZeroToNine}` | `3${0 | 1}`;
 type DateString = `${DateStringYearMonth}-${DateStringDay}`;
 export type PartialDateString =
-  | DateStringYear
-  | DateStringYearMonth
-  | DateString;
+  DateStringYear | DateStringYearMonth | DateString;
 
 export const isPartialDateString = (
   value: string,
